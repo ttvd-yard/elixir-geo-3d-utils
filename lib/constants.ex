0eb1fc0597980ec61551959d1fdf634eca23381b
Mykola Konyk <mykola@konyk.org>
2015-12-03 02:40:19 -0500

    constants: Adding.

---
+++ lib/constants.ex
@@ -0,0 +1,18 @@
+defmodule Geo3d.Constants do
+
+  @spec epsilon :: float
+  def epsilon, do: 1.0e-10
+
+  @spec pi :: float
+  def pi, do: 3.14159265358979323846
+
+  @spec half_pi :: float
+  def half_pi, do: 1.5707963267948966
+
+  @spec two_pi :: float
+  def two_pi, do: 6.283185307179586
+
+  @spec inf :: float
+  def inf, do: 1.79769e308
+
+end
